6c226af18ecdcbe4c65e87376de1cc85ec673071
KINGCBERTON <stevekim724@gmail.com>
2023-12-07 19:11:31 +0300

    Api

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:realtimedb/controller/realtime_coontoller.dart';
 
-
-
 import 'homepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
-   
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: "AIzaSyAmOB_-8DIEIwJEOvrM2a8C6RE-KbvSDDc",
-        projectId: "educative-9",
-        storageBucket: "educative-9.appspot.com",
-         databaseURL: "https://educative-9-default-rtdb.firebaseio.com",
-        messagingSenderId: "14205101883",
-        appId:"1:14205101883:web:7c0db5d88bc2dc0310791e",
+        apiKey: "{{API_Key}}",
+        projectId: "{{projectId}}",
+        storageBucket: "{{storageBucket}}",
+        databaseURL: "{{databaseURL}}",
+        messagingSenderId: "{{messagingSenderId}}",
+        appId: "{{appId}}",
       ),
     ).then((value) => Get.put(Realtime()));
     runApp(const MyApp());
   } else {
-    
     await Firebase.initializeApp().then((value) => Get.put(Realtime()));
   }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   // This widget is the root of your application.
   @override
@@ -41,11 +36,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
-      ), 
+      ),
       getPages: [
-   //add pages here
+        //add pages here
       ],
-     home: const HomePage(),
+      home: const HomePage(),
     );
   }
 }
