aa5721302902ae0ef8df84264f47686f04a8af3b
KINGCBERTON <stevekim724@gmail.com>
2023-12-11 21:26:06 +0300

    icon

--- lib/post.dart
+++ lib/post.dart
@@ -28,7 +28,7 @@ class PostCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   CircleAvatar(
-                    backgroundImage: AssetImage('images/splash.jpg'),
+                    child: const Icon(Icons.person),
                   )
                 ],
               ),
