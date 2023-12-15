4c0f5d619e23dd9e8b47bbd67dbe1516c4ba2f16
KINGCBERTON <stevekim724@gmail.com>
2023-12-15 21:03:06 +0300

    fin edet

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -149,9 +149,7 @@ class MyBottomNavigationBar extends StatelessWidget {
       constraints: const BoxConstraints(maxWidth: 300),
       height: 70,
       decoration: BoxDecoration(
-         image: const DecorationImage(
-            image: AssetImage("assets/images/Splash.jpg"), 
-            fit: BoxFit.fill),
+   
         borderRadius: BorderRadius.circular(9),
         boxShadow: [
           BoxShadow(
